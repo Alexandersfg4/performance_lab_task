04daf61240d7b1dac2b88a8ce65a263175193c50
alex <alexanderbortnikov@mail.com>
2023-01-17 14:37:41 +0300

    Added working task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -2,19 +2,23 @@ import sys
 import json
 
 
-def make_report(dict_tests_: dict, dict_values_: dict) -> dict:
+def make_pairs(vals):
+    return {v.get("id"): v.get("value") for v in vals}
+
+
+def set_up_value(tes, pairs_):
+    for t in tes:
+        t["value"] = pairs_.get(t.get("id"))
+        if t.get("values"):
+            t = set_up_value(t.get("values"), pairs_)
+    return tes
+
+
+def make_report(dict_tests_, dict_values_):
     tests_ = dict_tests_.get("tests")
     values_ = dict_values_.get("values")
-    for test in tests_:
-        id_ = test.get("id")
-        for value in values_:
-            if value.get("id") == id_:
-                test["value"] = value.get("value")
-            if value.get("values"):
-                if value.get("id") == id_:
-                    test["value"] = value.get("value")
-
-    return tests_
+    pairs = make_pairs(values_)
+    return set_up_value(tests_, pairs)
 
 
 if __name__ == "__main__":
@@ -28,5 +32,5 @@ if __name__ == "__main__":
     dict_values = json.loads(content_values)
     output = make_report(dict_tests, dict_values)
     print(output)
-    # with open("report.json", "w") as report:
-    #     report.write(json.dump(output))
+    with open("report.json", "w") as report:
+        report.write(json.dumps(output, indent=4))
