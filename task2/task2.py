91188384fdd81250d6aa9ce4266be2cc6ccc2ae9
alex <alexanderbortnikov@mail.com>
2023-01-18 12:54:28 +0300

    Added working task2.py

--- task2/task2.py
+++ task2/task2.py
@@ -1,21 +1,29 @@
-import itertools
 import sys
-import math
 
 
-def find_point(x, y, radius):
-    hypotenuse = math.sqrt(x * 2 + y * 2)
-    for value in itertools.cycle(range(1, int(n))):
-        if value:
-            break
-        path.append(value)
-    return path
+def find_point(x, y, circle_x, circle_y, circle_r):
+    # x ^ 2 + y ^ 2 <= R ^ 2
+    action = (x - circle_x) ** 2 + (y - circle_y) ** 2
+    circle_r_square = circle_r ** 2
+    if action == circle_r_square:
+        return 0
+    elif action < circle_r_square:
+        return 1
+    else:
+        return 2
 
 
 if __name__ == "__main__":
-    print(sys.argv)
     center_file = sys.argv[1]
     points_file = sys.argv[2]
     with open(center_file, "r") as f:
-        file = f.read()
-    print(file)
+        circle_points = f.readline()
+        circle_radius = f.readline()
+    circle_x_point, circle_y_point = circle_points.split()
+    with open(points_file, "r") as f:
+        points = f.readlines()
+    for point in points:
+        point_x, point_y = point.split()
+        result = find_point(float(point_x), float(point_y), float(circle_x_point),
+                            float(circle_y_point), int(circle_radius))
+        print(result)
